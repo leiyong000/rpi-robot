12f77f01837b3c7e7f130e82b22f16614ae8edd1
leiyong000 <61767931+leiyong000@users.noreply.github.com>
2022-05-27 08:28:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ sudo apt install python3-pip -y
 cd /tmp
 wget https://github.com/joan2937/pigpio/archive/master.zip
 unzip master.zip
+rm master.zip
 cd pigpio-master
 make
 sudo make install
@@ -20,9 +21,13 @@ wget https://github.com/jacksonliam/mjpg-streamer/archive/master.zip
 unzip master.zip
 rm master.zip
 cd mjpg-streamer-master/mjpg-streamer-experimental
-sudo apt-get install cmake libjpeg8-dev -y
+sudo apt-get install cmake libjpeg9-dev -y
 sudo apt-get install gcc g++ -y
 make
 sudo make install
+
+cd /tmp
+wget https://github.com/nknytk/built-onnxruntime-for-raspberrypi-linux/raw/master/wheels/bullseye/onnxruntime-1.11.1-cp39-cp39-linux_armv7l.whl
+pip3 install onnxruntime-1.11.1-cp39-cp39-linux_armv7l.whl
 
 pip3 install -r requirements.txt
